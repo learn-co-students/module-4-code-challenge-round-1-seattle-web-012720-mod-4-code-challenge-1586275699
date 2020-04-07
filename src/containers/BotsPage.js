a350789a3b7f9ffc0d55a06b17c8e0008099ca3b
unclezap <zeb.blackwell@gmail.com>
2020-04-07 10:09:14 -0700

    checkpoint with two comments

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -27,11 +27,13 @@ class BotsPage extends React.Component {
     })
   }
 
+  //yeah this is named a little confusingly right now but the functionality works
   draftBot = (robot) => {
-    console.log("drafted!")
-    console.log(robot)
+    // console.log("drafted!")
+    // console.log(robot)
     if (this.state.enlistedBots.find(enlistedBot => enlistedBot.id === robot.id)) {
-      console.log("already there!")
+      // console.log("already there!")
+      //I think technically this is only supposed to work if you're clicking on the card in the enlisted bots row, but that requires a little more fiddling
       this.setState((previousState) => {
         let newArmy = previousState.enlistedBots.filter((enlistedBot => enlistedBot.id !== robot.id))
         return {
